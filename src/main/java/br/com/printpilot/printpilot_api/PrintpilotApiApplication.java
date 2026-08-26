09b020a4a9eac371a1b5a9321fa73147def8b34a
Matheus Maciel <srmatheusmaciel@hotmail.com>
2026-08-26 19:01:36 -0300

    first commit

---
+++ src/main/java/br/com/printpilot/printpilot_api/PrintpilotApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.printpilot.printpilot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintpilotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintpilotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/printpilot/printpilot_api/PrintpilotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.printpilot.printpilot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintpilotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
